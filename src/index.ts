7ec7199fbcab96f0e60cbe71c2c2690847fcd8c5
Jesse Luoto <jesse.luoto@gmail.com>
2025-05-24 23:03:49 +0300

    Add cache headers

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,20 @@ const app = express();
 const generateResponseThrottled = throttled(generateResponse);
 
 app.get("/", async (req, res) => {
+  res.set({
+    "Cache-Control": "public, max-age=1",
+    Expires: new Date(Date.now() + 1000).toUTCString(),
+    "X-Content-Type-Options": "nosniff",
+    "X-Frame-Options": "DENY",
+    "X-XSS-Protection": "1; mode=block",
+    "Referrer-Policy": "strict-origin-when-cross-origin",
+    "Content-Security-Policy":
+      "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+    "Content-Type": "text/html; charset=utf-8",
+    "Content-Language": "en",
+  });
   const response = await generateResponseThrottled();
+
   res.send(response);
 });
 
