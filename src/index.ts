54f659f911656702bdb5cf11e1d8a0f45c19f2e1
Jesse Luoto <jesse.luoto@gmail.com>
2025-05-24 23:25:14 +0300

    Remove CSP, pages might generate other deps

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,6 @@ app.get("/", async (req, res) => {
     "X-Frame-Options": "DENY",
     "X-XSS-Protection": "1; mode=block",
     "Referrer-Policy": "strict-origin-when-cross-origin",
-    "Content-Security-Policy":
-      "default-src 'self'; script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; img-src 'self' data: https://gravatar.com;",
     "Content-Type": "text/html; charset=utf-8",
     "Content-Language": "en",
   });
